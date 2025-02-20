f1c3dd66e1e08040e8b9262fa7c0569a65d68b90
ulugbekAbdullayev <ulu23503@gmail.com>
2025-02-20 19:06:57 +0500

    learn

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client'
 import Navbar from './assets/Navbar.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import About from './assets/About.jsx'
-import Footer from './assets/footer.jsx'
+import Footer from './assets/Footer.jsx'
 import Section from './assets/Section.jsx'
 import Contact from './assets/Contact.jsx'
 
@@ -19,7 +19,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    <Footer />
+    <Footer/>
 
 
     </BrowserRouter>
